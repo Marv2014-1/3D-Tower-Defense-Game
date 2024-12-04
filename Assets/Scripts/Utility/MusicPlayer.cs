ebabbf83064d6b77e25ddce00d6c59b5cb63d46f
bbailey0628 <bbailey0628@hotmail.com>
2024-12-04 03:42:55 -0500

    Imported select RS Assets
    Imported MainMenu and Story scenes
    Imported UI, Save, Score, and Audio prefabs
    Adjusted imported assets to fit project

---
+++ Assets/Scripts/Utility/MusicPlayer.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip menuSong, storySong;
+    public static MusicPlayer instance;
+    public static AudioSource music;
+    public AudioMixer audioMixer;
+    public float masterVolume = 1f, musicVolume = 1f, sfxVolume = 1f;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Allows the music player to listen for scene changes
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        music = this.GetComponent<AudioSource>();
+        audioMixer = transform.GetComponent<AudioSource>().outputAudioMixerGroup.audioMixer;
+    }
+
+    // Calls everytime a scene changes and changes the music track
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        string sceneName = scene.name;
+
+        if (sceneName == "MainMenu")
+        {
+            music.clip = menuSong;
+        }
+        else if (sceneName == "Story")
+        {
+            music.clip = storySong;
+        } else
+        {
+            music.clip = null;
+        }
+
+        AudioListener.pause = false;
+        music.Play();
+    }
+
+    public void MasterVolume(float volume)
+    {
+        masterVolume = volume;
+        audioMixer.SetFloat("masterVol", Mathf.Log10(volume) * 20);
+    }
+
+    public void MusicVolume(float volume)
+    {
+        musicVolume = volume;
+        audioMixer.SetFloat("musicVol", Mathf.Log10(volume) * 20);
+    }
+
+    public void SFXVolume(float volume)
+    {
+        sfxVolume = volume;
+        audioMixer.SetFloat("sfxVol", Mathf.Log10(volume) * 20); 
+    }
+
+    // called when the game is terminated
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+}
